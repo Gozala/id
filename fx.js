c979ef124d92a9b21b95981f09437490bea41fb4
Irakli Gozalishvili <rfobic@gmail.com>
2018-08-27 11:03:18 -0700

    Rewrite all with virtual-dom

--- fx.js
+++
@@ -1,8 +0,0 @@
-const Void = _ => {}
-export const nofx = state => [state, Void]
-export const fx = (state, task) => [state, task]
-export const chain = (...tasks) => async mailbox => {
-  for (const task of tasks) {
-    await task(mailbox)
-  }
-}
