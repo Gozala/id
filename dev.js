2b73dac485390c7721e72600e5885fc54cedbe31
Irakli Gozalishvili <rfobic@gmail.com>
2018-08-27 16:59:12 -0700

    Only load when on dat protocol

--- dev.js
+++ dev.js
@@ -1,18 +1,20 @@
 ;(async () => {
-  const archive = await DatArchive.load(`dat://${location.host}`)
-  const iframe = document.createElement("iframe")
-  iframe.style.display = "none"
-  iframe.src = `lib?time=${Date.now()}`
-
-  const watcher = archive.watch(["*.js", "**/*.js"])
-
-  watcher.addEventListener("changed", ({ path }) => {
-    console.log(`Reload application since document has changed ${path}`)
+  if (location.protocol === "dat:") {
+    const archive = await DatArchive.load(`dat://${location.host}`)
+    const iframe = document.createElement("iframe")
+    iframe.style.display = "none"
     iframe.src = `lib?time=${Date.now()}`
-    if (iframe.parentNode == null) {
-      document.documentElement.appendChild(iframe)
-    }
-  })
 
-  document.documentElement.appendChild(iframe)
+    const watcher = archive.watch(["*.js", "**/*.js"])
+
+    watcher.addEventListener("changed", ({ path }) => {
+      console.log(`Reload application since document has changed ${path}`)
+      iframe.src = `lib?time=${Date.now()}`
+      if (iframe.parentNode == null) {
+        document.documentElement.appendChild(iframe)
+      }
+    })
+
+    document.documentElement.appendChild(iframe)
+  }
 })()
